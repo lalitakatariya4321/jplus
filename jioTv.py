0e75f3b2719c0300bbaf908c3c78a5dcac3be0e8
Ajit Singh Yadav <37411261+AjitGE@users.noreply.github.com>
2024-08-03 15:01:18 +0530

    Update jioTv.py

--- jioTv.py
+++ jioTv.py
@@ -2,7 +2,7 @@ import requests;
 import json;
 
 global allChannels
-m3ustr = ''
+m3ustr = '#EXTM3U x-tvg-url="https://avkb.short.gy/epg.xml.gz"\n'
 
 apiUrl="https://fox.toxic-gang.xyz/jplus/key/$ToReplace"
 
@@ -17,15 +17,16 @@ for channelList in allchannels:
         print(channelList['channel_id'])
         response = requests.request("GET",apiUrl.replace("$ToReplace",str(channelList['channel_id'])))
         jsonResult= response.json()
-        streamUrl=jsonResult[0]['data']['initialUrl']
-        hdntl=jsonResult[0]['hdntl']
-        clearKeys=str(jsonResult[0]["keys"])
-        channelLogo= "https://jiotv.catchup.cdn.jio.com/dare_images/images/"+channelList['logoUrl']
-        extvlcopt="#EXTVLCOPT:plaYtv/7.1.5 (Linux;Android 13) ExoPlayerLib/2.11.7\n"
-        kodiProps= '#KODIPROP:inputstream.adaptive.license_type=clearkey\n'+'#KODIPROP:inputstream.adaptive.license_key='+clearKeys.replace("'",'"')+'\n'
-        extHttp= '#EXTHTTP:{"Cookie":"'+hdntl+'"}\n'
-        m3ustr += "#EXTINF:-1 "
-        m3ustr += "tvg-id="+ "\"" + str(channelList['channel_id']) + "\" " + "group-title=" + "\"" + channelList['channelCategoryId'] + "\" " "tvg-logo=\""+channelLogo+ "\"," + channelList['channel_name'] +"\n"+ kodiProps + extHttp +extvlcopt+streamUrl+'|Cookie:'+hdntl+"\n\n"
+        if jsonResult[0]['data']['initialUrl'] != "Error: MPD URL not found for the given tvg-id.":
+                streamUrl=jsonResult[0]['data']['initialUrl']
+                hdntl=jsonResult[0]['hdntl']
+                clearKeys=str(jsonResult[0]["keys"])
+                channelLogo= "https://jiotv.catchup.cdn.jio.com/dare_images/images/"+channelList['logoUrl']
+                extvlcopt="#EXTVLCOPT:plaYtv/7.1.5 (Linux;Android 13) ExoPlayerLib/2.11.7\n"
+                kodiProps= '#KODIPROP:inputstream.adaptive.license_type=clearkey\n'+'#KODIPROP:inputstream.adaptive.license_key='+clearKeys.replace("'",'"')+'\n'
+                extHttp= '#EXTHTTP:{"Cookie":"'+hdntl+'"}\n'
+                m3ustr += "#EXTINF:-1 "
+                m3ustr += "tvg-id="+ "\"" + str(channelList['channel_id']) + "\" " + "group-title=" + "\"" + channelList['channelCategoryId'] + "\" " "tvg-logo=\""+channelLogo+ "\"," + channelList['channel_name'] +"\n"+ kodiProps + extHttp +extvlcopt+streamUrl+'|Cookie:'+hdntl+"\n\n"
 
 
 with open("allChannelPlaylist.m3u", "w") as allChannelPlaylistFile:

9210c4aacbfa7213e4016200bb16c6f90341efe8
Ajit Singh Yadav <37411261+AjitGE@users.noreply.github.com>
2024-08-03 13:40:32 +0530

    Update jioTv.py

--- jioTv.py
+++ jioTv.py
@@ -2,7 +2,7 @@ import requests;
 import json;
 
 global allChannels
-m3ustr = '#EXTM3U x-tvg-url="https://avkb.short.gy/epg.xml.gz"\n'
+m3ustr = ''
 
 apiUrl="https://fox.toxic-gang.xyz/jplus/key/$ToReplace"
 
@@ -20,11 +20,12 @@ for channelList in allchannels:
         streamUrl=jsonResult[0]['data']['initialUrl']
         hdntl=jsonResult[0]['hdntl']
         clearKeys=str(jsonResult[0]["keys"])
+        channelLogo= "https://jiotv.catchup.cdn.jio.com/dare_images/images/"+channelList['logoUrl']
         extvlcopt="#EXTVLCOPT:plaYtv/7.1.5 (Linux;Android 13) ExoPlayerLib/2.11.7\n"
         kodiProps= '#KODIPROP:inputstream.adaptive.license_type=clearkey\n'+'#KODIPROP:inputstream.adaptive.license_key='+clearKeys.replace("'",'"')+'\n'
         extHttp= '#EXTHTTP:{"Cookie":"'+hdntl+'"}\n'
         m3ustr += "#EXTINF:-1 "
-        m3ustr += "tvg-id="+ "\"" + str(channelList['channel_id']) + "\" " + "group-title=" + "\"" + channelList['channelCategoryId'] + "\" " "tvg-logo=\""+str(channelList['logoUrl']) + "\"," + channelList['channel_name'] +"\n"+ kodiProps + extHttp +extvlcopt+streamUrl+'|Cookie:'+hdntl+"\n\n"
+        m3ustr += "tvg-id="+ "\"" + str(channelList['channel_id']) + "\" " + "group-title=" + "\"" + channelList['channelCategoryId'] + "\" " "tvg-logo=\""+channelLogo+ "\"," + channelList['channel_name'] +"\n"+ kodiProps + extHttp +extvlcopt+streamUrl+'|Cookie:'+hdntl+"\n\n"
 
 
 with open("allChannelPlaylist.m3u", "w") as allChannelPlaylistFile:
